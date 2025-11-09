0d76f8845ec3e5844453177768ddc70937f5045c
ErickDevv <111036821+ErickDevv@users.noreply.github.com>
2025-11-09 02:14:18 -0600

    Refactor GET function to remove unused request parameter

--- app/califica/route.ts
+++ app/califica/route.ts
@@ -1,7 +1,6 @@
-import { NextRequest } from 'next/server';
 import { redirect } from 'next/navigation';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
 
 const reviewUrl = 'https://g.page/r/CW17en6_Slf5EAI/review';
 
